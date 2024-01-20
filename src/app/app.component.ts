a0e54ef684b22307112d3801324c77d565900074
Samidha Vaity <samidhavaity@gmail.com>
2024-01-20 15:11:16 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {ReactiveFormsModule} from '@angular/forms';
+// import classes which are required for reactive forms
+import {FormBuilder,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ButtonsModule,AlertModule,BsDropdownModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers : [FormBuilder,Validators]
 })
 export class AppComponent {
-  title = 'Form';
+// Inject FormBuilder service
+constructor(public fbobj : FormBuilder)
+{
+}
+
+MarvellousForm = this.fbobj.group(
+  {
+    firstname :['', [Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]+$")] ],
+    lastname :['', [Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]+$")] ],
+    phone :['', [Validators.required, Validators.pattern("[0-9 ]{10}")] ],
+    email:['',[Validators.required, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[.][a-zA-Z]{2,4}$")]]
+  }
+);
+
 }
